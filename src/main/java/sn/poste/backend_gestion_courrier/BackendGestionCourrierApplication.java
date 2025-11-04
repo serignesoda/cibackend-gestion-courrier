2a3d5d2e2e0bf82c4f81a3873c1a8fd9f3d2a281
roland <rolandjosue05@gmail.com>
2025-11-04 12:17:11 +0000

    initialisation de mon projet

---
+++ src/main/java/sn/poste/backend_gestion_courrier/BackendGestionCourrierApplication.java
@@ -0,0 +1,13 @@
+package sn.poste.backend_gestion_courrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionCourrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestionCourrierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/poste/backend_gestion_courrier/BackendGestionCourrierApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.poste.backend_gestion_courrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionCourrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
