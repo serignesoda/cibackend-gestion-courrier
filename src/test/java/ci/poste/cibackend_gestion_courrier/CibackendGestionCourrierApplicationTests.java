0bf93d7e4eb9e5bd6c2a4e28b4aee06c2acf6ae0
roland <rolandjosue05@gmail.com>
2025-11-04 13:39:41 +0000

    initial commit

---
+++ src/test/java/ci/poste/cibackend_gestion_courrier/CibackendGestionCourrierApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.poste.cibackend_gestion_courrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibackendGestionCourrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
